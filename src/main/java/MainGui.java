bde66929dcbaadfeb26188662cae4fbcd92da3ad
Joe Hilse <hilse.joe@INTERN.BUESSING.SCHULE>
2021-12-13 10:50:39 +0100

    minor cleanup

--- src/main/java/MainGui.java
+++ src/main/java/MainGui.java
@@ -18,7 +18,6 @@ import javax.swing.WindowConstants;
 public class MainGui
   extends JFrame
 {
-  private JPanel pages;
   private Map<Config, JComponent> configToComponentLookup = new HashMap();
 
   public MainGui()
@@ -35,14 +34,10 @@ public class MainGui
     setTitle("MainGui");
     setResizable(false);
     Container cp = getContentPane();
-    cp.setLayout(null);
+    cp.setLayout(new BorderLayout());
     
     int headerHeight = 25;
     int contentHeight = frameHeight - headerHeight;
-    
-    JPanel borderLayout = new JPanel();
-    borderLayout.setBounds(0, 0, frameWidth, frameHeight);
-    borderLayout.setLayout(new java.awt.BorderLayout());
 
     JPanel headerPane = new JPanel();
     headerPane.setBounds(0, 0, frameWidth, frameHeight);
@@ -50,20 +45,18 @@ public class MainGui
     headerLabel.setBounds(0, 0, 100, 17);
     headerPane.add(headerLabel);
     
-    borderLayout.add(headerPane, BorderLayout.PAGE_START);
+    cp.add(headerPane, BorderLayout.PAGE_START);
 
     Config initialConfig = new Config.RecipeViewer();
-    JComponent initialComponent = getComponent(initialConfig, borderLayout, 0, headerHeight, frameWidth, contentHeight);
+    JComponent initialComponent = getComponent(initialConfig, cp, 0, headerHeight, frameWidth, contentHeight);
     initialComponent.setBounds(0, headerHeight, frameWidth, contentHeight);
 
     configToComponentLookup.put(initialConfig, initialComponent);
 
     if(initialComponent != null)
     {
-      borderLayout.add(initialComponent, BorderLayout.CENTER);
+      cp.add(initialComponent, BorderLayout.CENTER);
     }
-    
-    cp.add(borderLayout);
 
     setVisible(true);
   }
@@ -73,7 +66,7 @@ public class MainGui
     new MainGui();
   }
 
-  private JComponent getComponent(Config config, JPanel borderLayout, int x, int y, int width, int height)
+  private JComponent getComponent(Config config, Container borderLayout, int x, int y, int width, int height)
   {
     if(config instanceof Config.RecipeViewer)
     {
@@ -81,14 +74,14 @@ public class MainGui
       recipeViewer.onNavigateToCreateRecipe(() -> navigateTo(new Config.CreateRecipe(), borderLayout, x, y, width, height));
       return new RecipeViewerGui(recipeViewer);
     }
-    else if(config instanceof Config.CreateRecipe)
+    if(config instanceof Config.CreateRecipe)
     {
       CreateRecipeController createRecipe = new CreateRecipeController();
       createRecipe.onNavigateToRecipeViewer(() -> navigateTo(new Config.RecipeViewer(), borderLayout, x, y, width, height));
       createRecipe.onNavigateToCreateIngredient(() -> navigateTo(new Config.CreateIngredient(), borderLayout, x, y, width, height));
       return new CreateRecipeGui(createRecipe);
     }
-    else if(config instanceof Config.CreateIngredient)
+    if(config instanceof Config.CreateIngredient)
     {
       CreateIngredientController createIngredient = new CreateIngredientController();
       createIngredient.onNavigateToCreateRecipe(() -> navigateTo(new Config.CreateRecipe(), borderLayout, x, y, width, height));
@@ -98,7 +91,7 @@ public class MainGui
     return null;
   }
 
-  private void navigateTo(Config config, JPanel borderLayout, int x, int y, int width, int height)
+  private void navigateTo(Config config, Container borderLayout, int x, int y, int width, int height)
   {
     JComponent nextComponent;
     if(configToComponentLookup.containsKey(config))
@@ -114,7 +107,7 @@ public class MainGui
     if(nextComponent != null)
     {
       nextComponent.setBounds(x, y, width, height);
-      borderLayout.remove(borderLayout.getComponentAt(x, y + 1));
+      borderLayout.remove(borderLayout.getComponentAt(x + 1, y + 1));
       borderLayout.add(nextComponent, BorderLayout.CENTER);
       borderLayout.validate();
     }
